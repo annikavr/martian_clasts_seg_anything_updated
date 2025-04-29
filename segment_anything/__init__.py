6bf2166a2cfbbfdd6621049c7f9cd55f430d970f
annikavr <annikavr@g.ucla.edu>
2025-04-28 23:16:05 -0700

    Update __init__.py

--- segment_anything/__init__.py
+++ segment_anything/__init__.py
@@ -4,7 +4,8 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .build_sam import build_sam_vit_h, build_sam_vit_l, build_sam_vit_b, sam_model_registry
+from .build_sam import build_sam_vit_h, build_sam_vit_l, build_sam_vit_b
+
 
 from .predictor import SamPredictor
 from .automatic_mask_generator import SamAutomaticMaskGenerator
